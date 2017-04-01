f54dc013b891fbed491e0ef8faec3d46705f45b3
Jamie Bishop <jamie@nullpixel.uk>
2017-04-01 23:31:07 +0100

    wat a cuck

---
+++ config/passport.js
@@ -0,0 +1,19 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+
+// Get user model
+const User = require('../models/user');
+const config = require('../config/database');
+
+module.exports = function(passport) {
+    var opts = {};
+    opts.secretsOrKey = config.secret;
+    passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+        User.findOne({id: jwt_payload.id}, function(err, user) {
+            if (err) return done(err, false);
+            if (user) return done(null, user);
+            else {
+                done(null, false);
+            }
+        });
+    }));
+}
