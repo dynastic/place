53794b0e42c897f8307390c394c7a9a29af08e98
AppleBetas <applebetas@icloud.com>
2017-04-01 23:00:02 -0400

    Support for user in header

--- util/ResponseFactory.js
+++ util/ResponseFactory.js
@@ -1,6 +1,7 @@
 module.exports = {
     sendRenderedResponse: function(template, req, res, data) {
         var sendData = { url: req.url, path: req.path };
+        if(typeof req.user !== undefined) sendData.user = req.user;
         if(typeof data !== 'undefined') {
             if(data) sendData = Object.assign({}, sendData, data);
         }
