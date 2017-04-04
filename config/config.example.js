981e9b826b859baa4911e34b04f80c380a00d859
AppleBetas <applebetas@icloud.com>
2017-04-04 18:14:46 -0400

    Add debug config option, debug label

--- config/config.example.js
+++ config/config.example.js
@@ -3,6 +3,7 @@ module.exports = {
     'database': 'mongodb://localhost/place',
     'port': 3000,
     'onlyListenLocal': true,
+    'debug': false,
     'placeTimeout': 300,
     'recaptcha': {
         'siteKey': "",
--- util/ResponseFactory.js
+++ util/ResponseFactory.js
@@ -1,6 +1,8 @@
+const config = require("../config/config");
+
 module.exports = {
     sendRenderedResponse: function(template, req, res, data) {
-        var sendData = { url: req.url, path: req.path };
+        var sendData = { url: req.url, path: req.path, config: config };
         if (typeof req.user !== undefined) sendData.user = req.user;
         if (typeof data !== 'undefined') {
             if (data) sendData = Object.assign({}, sendData, data);
