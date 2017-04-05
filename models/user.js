e070c24eee27b8533f3e7e50826178e5126b02aa
Mstrodl <ipadlover8322@gmail.com>
2017-04-05 16:24:14 -0400

    *Whoopsies*

--- models/user.js
+++ models/user.js
@@ -95,16 +95,14 @@ UserSchema.methods.toInfo = function() {
 
 UserSchema.methods.loginError = function() {
     if(this.banned === true) return { message: "You are banned from using this service due to violations of the rules.", code: "banned" }
-}
     return null;
+}
 UserSchema.methods.setUserName = function(username, callback, usernameSet) {
     if(!UserSchema.statics.isValidUsername(username)) return callback({ message: "That username cannot be used. Usernames must be 3-20 characters in length and may only consist of letters, numbers, underscores, and dashes.", code: "username_taken" });
     this.name = username;
-    this.userNameSet = true;
+    this.usernameSet = true;
     this.save(function(err) {
-        console.log(err);
         if(err) return callback({ message: "That username already exists.", code: "username_taken" });
-        console.log("Saved(?)");
         return callback();
     });
 }
