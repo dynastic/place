6a6d1ec5afda16f4184d87835b1ce44914348fa4
AppleBetas <applebetas@icloud.com>
2017-04-04 21:19:26 -0400

    Better default secret

--- config/config.example.js
+++ config/config.example.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'secret': 'conk',
+    'secret': 'CHANGETHISDONTUSETHISITSINSECURE', /* <------- CHANGE THIS DONT USE THE DEFAULT YOU'LL GET HACKED AND DIE 100% */
     'database': 'mongodb://localhost/place',
     'port': 3000,
     'onlyListenLocal': true,
