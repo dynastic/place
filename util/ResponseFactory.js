249b72c98b9e5221424b3f945322d6ccfe486374
AppleBetas <applebetas@icloud.com>
2017-04-07 22:16:09 -0400

    More checks

--- util/ResponseFactory.js
+++ util/ResponseFactory.js
@@ -1,9 +1,10 @@
 const config = require("../config/config");
+const fs = require("fs");
 
 function ResponseFactory(root = "") {
     return {
         sendRenderedResponse: function(template, req, res, data, mimeType = "text/html") {
-            var sendData = { url: req.url, path: root + req.path, config: config };
+            var sendData = { url: req.url, path: root + req.path, config: config, fs: fs };
             if (typeof req.user !== undefined) sendData.user = req.user;
             if (typeof data !== 'undefined') {
                 if (data) sendData = Object.assign({}, sendData, data);
