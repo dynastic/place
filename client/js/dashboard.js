9eb4243046655a9bf45cc48d912a2ba8e1c11466
AppleBetas <applebetas@icloud.com>
2017-04-08 01:32:57 -0400

    Auto-update dashboard

--- client/js/dashboard.js
+++ client/js/dashboard.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
+    reloadData();
+});
+
+function reloadData() {
     $.get("/api/admin/stats", function(data) {
         $("#users-online").text(data.stats.online.toLocaleString());
         $("#new-accounts").text(data.stats.signups24h.toLocaleString());
         $("#pixels-placed").text(data.stats.pixelsPlaced24h.toLocaleString());
         $("#pixels-placed-2").text(data.stats.pixelsPerMin.toLocaleString());
+    }).always(function() {
+        setTimeout(reloadData, 2000);
     });
-});
+}
