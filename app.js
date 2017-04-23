76b5ac21d5f425dc20d8385b87de567c2417dd59
AppleBetas <applebetas@icloud.com>
2017-04-23 13:49:58 -0400

    Check for enabled recaptcha, don't crash script if client-side JS has an error

--- app.js
+++ app.js
@@ -22,6 +22,12 @@ let paths = {
 var app = {};
 app.config = config;
 app.temporaryUserInfo = TemporaryUserInfo;
+app.enableCaptcha = false;
+if(typeof app.config.recaptcha !== 'undefined') {
+    if(typeof app.config.recaptcha.siteKey !== 'undefined' && typeof app.config.recaptcha.secretKey !== 'undefined') {
+        app.enableCaptcha = app.config.recaptcha.siteKey != "" && app.config.recaptcha.secretKey != "";
+    }
+}
 
 // Get image handler
 app.paintingHandler = paintingHandler(app);
@@ -55,26 +61,28 @@ app.websocketServer = new WebsocketServer(app, app.server);
 mongoose.connect(config.database);
 
 // Clean existing built JS
-gulp.task('clean', function() {
-    return del(['public/js/build']);
-});
+gulp.task('clean', () => del(['public/js/build']));
+
+function swallowError(error) {
+    console.error("An error occurred while trying to process JavaScript: " + error);
+    this.emit("end");
+}
 
 // Process JavaScript
 gulp.task('scripts', ['clean'], (cb) => {
     console.log("Processing JavaScript...");
     let t = gulp.src(paths.scripts.src)
-    t = t.pipe(babel({
-        presets: ['es2015']
-    }))
+    t = t.pipe(babel({ presets: ['es2015'] }));
+    t = t.on("error", swallowError);
     if(!config.debug) t = t.pipe(uglify());
-    t.pipe(gulp.dest(paths.scripts.built));
+    t = t.on("error", swallowError);
+    t = t.pipe(gulp.dest(paths.scripts.built));
+    t = t.on("end", () => console.log("Finished processing JavaScript."));
     return t;
 });
 
 // Rerun the task when a file changes 
-gulp.task('watch', () => {
-    gulp.watch(paths.scripts.src, ['scripts']);
-});
+gulp.task('watch', () => gulp.watch(paths.scripts.src, ['scripts']));
 
 gulp.task('default', ['watch', 'scripts']);
 gulp.start(['watch', 'scripts'])
