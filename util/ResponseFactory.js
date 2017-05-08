da802ccd893068f95eff782249d8a8d61e4943da
AppleBetas <applebetas@icloud.com>
2017-05-07 23:57:23 -0400

    Auth redirect URLs

--- util/ResponseFactory.js
+++ util/ResponseFactory.js
@@ -4,7 +4,9 @@ const fs = require("fs");
 function ResponseFactory(app, root = "") {
     return {
         sendRenderedResponse: function(template, req, res, data, mimeType = "text/html") {
-            var sendData = { url: req.url, path: root + req.path, config: config, fs: fs, renderCaptcha: () => app.recaptcha.render() };
+            var path = root + req.path;
+            var redirectURLPart = req.path == "/signin" || req.path == "/signup" ? "" : encodeURIComponent(req.url.substr(1));
+            var sendData = { url: req.url, path: path, config: config, fs: fs, renderCaptcha: () => app.recaptcha.render(), redirectURLPart: redirectURLPart };
             if (typeof req.user !== undefined) sendData.user = req.user;
             if (typeof data !== 'undefined') {
                 if (data) sendData = Object.assign({}, sendData, data);
