71460868783fc0012235ead9eae42afa170f1f2d
AppleBetas <applebetas@icloud.com>
2017-06-10 23:49:56 -0400

    Allow client to periodically reload leaderboard

--- client/js/place.js
+++ client/js/place.js
@@ -280,6 +280,9 @@ var place = {
 
         this.setupChat();
         this.loadLeaderboard();
+        setInterval(function() {
+            app.loadLeaderboard();
+        }, 30000);
         this.updateAuthLinks();
     },
 
