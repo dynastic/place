97c2e5afd5e5df71115d31c4bd394b9fc3778f5c
AppleBetas <applebetas@icloud.com>
2017-06-22 10:10:19 -0400

    Top 5 flair colour

--- client/js/place.js
+++ client/js/place.js
@@ -876,7 +876,7 @@ var place = {
                     var rank = data.pixel.user.statistics.leaderboardRank;
                     if(rank !== null) {
                         popover.find(".rank-container").show();
-                        popover.find(".rank-label").removeClass("label-info label-success").addClass(rank <= 25 ? "label-success" : "label-info").text(`Ranked #${rank.toLocaleString()}`);
+                        popover.find(".rank-label").removeClass("label-info label-success").addClass(`label-${rank <= 25 ? (rank <= 5 ? "danger" : "success") : "info"}`).text(`Ranked #${rank.toLocaleString()}`);
                         
                     } else popover.find(".rank-container").hide();
                     if (data.pixel.user.admin) popover.find("#pixel-badge").show().text("Admin");
