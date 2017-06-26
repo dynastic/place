582ff79515161147201412895d1dca1c11ddf506
AppleBetas <applebetas@icloud.com>
2017-06-26 16:46:56 -0400

    We don't need to log this

--- util/ResponseFactory.js
+++ util/ResponseFactory.js
@@ -4,7 +4,6 @@ function ResponseFactory(app, root = "") {
     return {
         sendRenderedResponse: function(template, req, res, data, mimeType = "text/html") {
             var resources = req.place.moduleManager.getResourcesFromModules(req);
-            console.log(resources);
             var path = root + req.path;
             var redirectURLPart = req.path == "/signin" || req.path == "/signup" ? "" : encodeURIComponent(req.url.substr(1));
             var sendData = { url: req.url, path: path, config: app.config, fs: fs, renderCaptcha: () => app.recaptcha.render(), redirectURLPart: redirectURLPart, moduleManager: req.place.moduleManager, resources: resources };
