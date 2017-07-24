adae144a4756957fe3a42849db42c6ceaaee170f
Jamie Bishop <jamie@nullpixel.uk>
2017-07-24 19:47:51 +0100

    hotfix

--- scripts/resize.js
+++ scripts/resize.js
@@ -8,25 +8,35 @@ console.log('USE WITH EXTREME CAUTION');
 console.log('PLEASE BACKUP YOUR DATABASE BEFORE RUNNING THIS TOOL');
 console.log('--------------------------------');
 
+// Flag variables
+var doneReading = false;
+let i = 0;
+let saved = 0;
+
 mongoose.connect(config.database).then(() => console.info('Connected to database'));
 
 let cursor = Pixel.find().cursor();
 
 cursor.on('data', (pixel) => {
+    i++;
     let o = pixel;
 
-    pixel.xPos = o.xPos += 200
-    pixel.yPos = o.yPos += 200
+    pixel.xPos += 200
+    pixel.yPos += 200
     
-    pixel.save((err, n) => {
+    pixel.save(function(err, n) {
         if (err) return console.error("Error saving pixel " + err);
+        saved++;
+        if (i === saved && doneReading) {
+            console.log(`Updated ${i} pixels`);
+            process.exit();
+        }
         console.log(`Updated pixel (${o.xPos}, ${o.yPos}) to (${pixel.xPos}, ${pixel.yPos})`);
     });
 });
 
 cursor.on('close', function() {
-    console.log("Done shifting pixels");
-    process.exit();
+    doneReading = true;
 });
 
 cursor.on('error', function(err) {
