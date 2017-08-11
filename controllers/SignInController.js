6e4b52418688640d277dd0a1682d9ca456313736
AppleBetas <applebetas@icloud.com>
2017-08-11 11:31:21 -0400

    Fuck Travis

--- controllers/SignInController.js
+++ controllers/SignInController.js
@@ -4,7 +4,6 @@ exports.postSignIn = (req, res, next) => {
     require("../util/passport")(passport, req.place);
     if (req.user) return res.redirect("/");
     if (!req.body.username || !req.body.password) return res.json({success: false, error: {message: "A username and password are required."}});
-    var redirectURL = typeof req.query.redirectURL !== "undefined" ? req.query.redirectURL : null;
     passport.authenticate("local", function(err, user, info) {
         if (!user) return res.json({success: false, error: info.error || {message: "A username and password are required."}});
         req.login(user, function(err) {
