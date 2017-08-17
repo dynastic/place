4bbe2c45c4f43b5ce25f09fc8f457deb1a91f362
AppleBetas <applebetas@icloud.com>
2017-08-17 01:05:38 -0400

    Try and fix TOTP

--- controllers/TOTPSetupController.js
+++ controllers/TOTPSetupController.js
@@ -14,7 +14,10 @@ exports.getTOTPSetup = function(req, res, next) {
 exports.postTOTPSetup = function(req, res, next) {
     if(req.user.twoFactorAuthEnabled()) return res.status(403).json({success: false, error: {message: "You already have two-factor authentication set up on your account.", code: "totp_already_setup"}});
     if(!req.body.secret || !req.body.token) return res.status(400).json({success: false, error: {message: "You didn't provide the required data.", code: "bad_request"}});
-    if(!speakeasy.totp.verify({secret: req.body.secret, encoding: 'base32', token: req.body.token, step: 120})) return res.status(403).json({success: false, error: {message: "The token you inputted is not valid for the QR code on screen.", code: "invalid_totp_token"}});
+    if(!speakeasy.totp.verify({secret: req.body.secret, encoding: 'base32', token: req.body.token, window: 2})) {
+        console.log(`REJECTING TOTP CODE: "${req.body.token}"`)
+        return res.status(403).json({success: false, error: {message: "The token you inputted is not valid for the QR code on screen.", code: "invalid_totp_token"}});
+    }
     req.user.totpSecret = req.body.secret;
     req.user.save().then(() => res.json({success: true})).catch((err) => {
         req.place.reportError("Couldn't enable user TOTP auth: " + err);
