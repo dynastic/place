0f47a92a1532986986be9db801a33d413352f7bc
AppleBetas <applebetas@icloud.com>
2017-08-17 00:41:28 -0400

    Try TOTP fix

--- controllers/SignInController.js
+++ controllers/SignInController.js
@@ -9,7 +9,7 @@ exports.postSignIn = (req, res, next) => {
         if (!user) return res.status(403).json({success: false, error: info.error || {message: "A username and password are required."}});
         if (user.twoFactorAuthEnabled()) {
             if(!req.body.totpToken) return res.status(403).json({success: false, error: {code: "totp_needed", message: "Two-factor authentication is enabled for this account. Please specify your two-factor authentication token."}});
-            if(!speakeasy.totp.verify({ secret: user.totpSecret, encoding: 'base32', token: req.body.totpToken })) return res.status(403).json({success: false, error: {code: "invalid_totp", message: "We couldn't sign you in with that two-factor authentication token. Make sure you're entering the right code and it is updated."}});
+            if(!speakeasy.totp.verify({ secret: user.totpSecret, encoding: 'base32', token: req.body.totpToken, step: 120 })) return res.status(403).json({success: false, error: {code: "invalid_totp", message: "We couldn't sign you in with that two-factor authentication token. Make sure you're entering the right code and it is updated."}});
         }
         req.login(user, function(err) {
             if (err) return res.status(500).json({success: true, error: {message: "An unknown error occurred."}});
