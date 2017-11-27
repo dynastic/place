b80d1b4028c992f34b65f51927449ba13c37893b
Jamie Bishop <jamie@nullpixel.uk>
2017-11-27 16:40:49 +0000

    Add maintenance configuration options to deal with situations better

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -7,11 +7,18 @@ const absoluteURLRegex = new RegExp('^(?:[a-z]+:)?(//)?', 'i');
 const User = require('../models/user');
 
 exports.postSignIn = (req, res, next) => {
+    const config = req.place.config;
     require("../util/passport")(passport, req.place);
     if (req.user) return res.redirect("/");
     if (!req.body.username || !req.body.password) return res.status(400).json({success: false, error: {message: "A username and password are required."}});
     passport.authenticate("local", function(err, user, info) {
         if (!user) return res.status(403).json({success: false, error: info.error || {message: "A username and password are required."}});
+        if (!user.admin && config.maintenance && !config.maintenance.allowSignins) return res.status(403).json({
+            success: false,
+            error: {
+                message: 'Logins disabled. Please do not call this endpoint any futher.'
+            }
+        });
         if (user.twoFactorAuthEnabled()) {
             if(!req.body.totpToken) return res.status(403).json({success: false, error: {code: "totp_needed", message: "Two-factor authentication is enabled for this account. Please specify your two-factor authentication token."}});
             if(!speakeasy.totp.verify({ secret: user.totpSecret, encoding: 'base32', token: req.body.totpToken, window: 6 })) return res.status(403).json({success: false, error: {code: "invalid_totp", message: "We couldn't sign you in with that two-factor authentication token. Make sure you're entering the right code and it is updated."}});
@@ -25,6 +32,13 @@ exports.postSignIn = (req, res, next) => {
 };
 
 exports.postSignUp = (req, res, next) => {
+    const config = req.place.config;
+    if (config.maintenance && !config.maintenance.allowSignups) return res.status(403).json({
+        success: false,
+        error: {
+            message: 'Signups disabled. Please do not call this endpoint any futher.'
+        }
+    });
     function sendError(error) {
         res.json({success: false, error: error || {message: "An unknown error occurred", code: "unknown_error"}});
     }
