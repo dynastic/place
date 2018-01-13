32e2c1277307e8a50b3ee77e976c98bc21d5960f
VideoStitch <videostitch@MacBook-Pro-de-VideoStitch.local>
2018-01-13 15:43:09 -0800

    - refactoring color to a single uint24 - adding boilerplate for uploading an array instead of a single pixel

--- solidity/cryptoplace.sol
+++ solidity/cryptoplace.sol
@@ -1,39 +1,50 @@
 pragma solidity ^0.4.19;
 
-
 contract CryptoPlace {
-    struct Color {
-        uint8 r;
-        uint8 g;
-        uint8 b;
 
-    }
+    uint16 CANVAS_WIDTH = 255 ;
+    uint16 CANVAS_HEIGHT = 255 ;
+
+    event TileUpdated(uint16 xy, uint24 color);
 
     struct Tile {
-        Color color;
+        uint24 color;
         address owner;
     }
 
     mapping (uint => Tile) grid;
 
-    function getTileColorRedAt(uint8 xy) public view returns (uint8) {
-        return grid[xy].color.r;
+    function getTileColor(uint16 xy) public view returns (uint24) {
+        return grid[xy].color;
     }
 
-    function getTileColorGreenAt(uint8 xy) public view returns (uint8) {
-        return grid[xy].color.g;
-    }
-
-    function getTileColorBlueAt(uint8 xy) public view returns (uint8) {
-        return grid[xy].color.b;
-    }
-
-    function getTileOwner(uint8 xy) public view returns (address) {
+    function getTileOwner(uint16 xy) public view returns (address) {
         return grid[xy].owner;
     }
 
-    function paintTile(uint8 xy, uint8 r, uint8 g, uint8 b) public {
-        Color memory c = Color({ r: r, g: g, b: b });
+    function paintTile(uint16 xy, uint24 c) public {
         grid[xy] = Tile({ color: c, owner: msg.sender });
+        TileUpdated(xy, c);
     }
+    
+    function xy_to_x_y(uint16 xy) internal view returns (uint16, uint16) {
+        return (xy % CANVAS_WIDTH, xy * CANVAS_WIDTH);
+    }
+    
+    function paintTileArray(uint16 xy_topleft, uint16 xy_bottomright, uint24[] pixels) public {
+        uint16 x_topleft;
+        uint16 y_topleft;
+        uint16 x_bottomright;
+        uint16 y_bottomright;
+        
+        (x_topleft,y_topleft) = xy_to_x_y(xy_topleft);
+        (x_bottomright,y_bottomright) = xy_to_x_y(xy_bottomright);
+        
+        uint16 widthArray = x_topleft - x_bottomright;
+        assert(widthArray > 1);
+        
+        uint16 heightArray = y_topleft - y_bottomright;
+        assert(heightArray > 1);        
+    }
+    
 }
