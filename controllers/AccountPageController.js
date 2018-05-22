dcbddad715139cd7e7d08610758f9c7d258f8041
AppleBetas <applebetas@icloud.com>
2018-05-22 12:34:54 -0400

    Fix profile pages

--- controllers/AccountPageController.js
+++ controllers/AccountPageController.js
@@ -12,18 +12,17 @@ exports.getAccountByID = (req, res, next) => {
 
 exports.getAccount = (req, res, next) => {
     User.findByUsername(req.params.username).then((user) => {
-        if(!user) return next();
-        if((user.banned || user.deactivated) && !(req.user.moderator || req.user.admin)) return next();
+        if (!user || ((user.banned || user.deactivated) && !(req.user && (req.user.moderator || req.user.admin)))) return next();
         user.getInfo(req.place).then(async (info) => {
-            if (req.user.admin) {
+            if (req.user && req.user.admin) {
                 const accessData = await user.getUniqueIPsAndUserAgents();
                 user.ipAddresses = accessData.ipAddresses;
                 user.userAgents = accessData.userAgents;
                 user.keys = accessData.keys;
             }
             return req.responseFactory.sendRenderedResponse("public/account", { profileUser: user, profileUserInfo: info, hasNewPassword: req.query.hasNewPassword });
-        }).catch((err) => next());
-    }).catch((err) => next());
+        });
+    })//.catch((err) => next());
 };
 
 exports.getAPIAccount = (req, res, next) => {
--- util/HTTPServer.js
+++ util/HTTPServer.js
@@ -93,7 +93,7 @@ function HTTPServer(app) {
                     return res.redirect("/#signin&loginerror=1&logintext=" + encodeURIComponent(user.loginError().message));
                 }
 
-                if(user) user.recordAccess(app, req.get("User-Agent"), req.get("X-Forwarded-For") || req.connection.remoteAddress, (typeof req.key !== "undefined" ? req.key : null));
+                if (user) user.recordAccess(app, req.get("User-Agent"), req.get("X-Forwarded-For") || req.connection.remoteAddress, (typeof req.key !== "undefined" ? req.key : null));
                 req.user = user;
                 next();
             }
